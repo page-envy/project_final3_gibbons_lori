8ab7659b83c84f293bdcb54b247550db593dc566
Lori Gibbons <lori.j.gibbons@gmail.com>
2017-07-30 15:48:18 -0400

    Add JavaScript document.images mouseover listener event

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+//Javascript Code
+//This should flip between two images upon hover of image in header in contact page
+var phoneImage = document.images[1];
+
+var phoneImageWithFood = document.images[2];
+
+phoneImage.addEventListener('mouseover', function() {
+    phoneImage.className = 'hide';
+    phoneImageWithFood.className = 'show'
+});
+phoneImageWithFood.addEventListener('mouseleave', function() {
+    phoneImage.className = 'show';
+    phoneImageWithFood.className = 'hide'
+});
+
+//JQuery Code
 $(document).ready(function() {
     $('.tooltip').tooltipster({
         theme: 'tooltipster-light',

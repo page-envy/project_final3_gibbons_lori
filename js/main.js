b7ae6b9cd5fb5882e2d6f37661322164a627e0f2
Lori Jane Gibbons <gibbons@uga.edu>
2017-07-31 13:21:08 -0400

    Correct JS linter errors

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 //Custom Javascript Code
 //This should flip between two images upon hover of image in header in contact page
-// var phoneImage = document.getElementById('monsterPhonePic');
-//
-// var phoneImageWithFood = document.getElementById('monsterPhonePicWithFood');
-//
-// phoneImage.addEventListener('mouseover', function() {
-//     phoneImage.className = 'hide';
-//     phoneImageWithFood.className = 'show'
-// });
-// phoneImageWithFood.addEventListener('mouseleave', function() {
-//     phoneImage.className = 'show';
-//     phoneImageWithFood.className = 'hide'
-// });
+var phoneImage = document.getElementById('monsterPhonePic');
+
+var phoneImageWithFood = document.getElementById('monsterPhonePicWithFood');
+
+phoneImage.addEventListener('mouseover', function () {
+    phoneImage.className = 'hide';
+    phoneImageWithFood.className = 'show';
+});
+phoneImageWithFood.addEventListener('mouseleave', function () {
+    phoneImage.className = 'show';
+    phoneImageWithFood.className = 'hide';
+});
 
 //JQuery Code
-$(document).ready(function() {
+$(document).ready(function () {
     //ToolTip Plugin
     //One of the required two to use
     // $('.tooltip').tooltipster({
@@ -23,13 +23,13 @@ $(document).ready(function() {
     // });
     //Backstretch Plugin
     //Second of the two required to use
-    $ ('#background-image').backstretch("http://dl.dropbox.com/u/515046/www/garfield-interior.jpg");
+    $('#background-image').backstretch('http://dl.dropbox.com/u/515046/www/garfield-interior.jpg');
     //Custom JQuery
     //This code will make paragraph text in about page red on mouseover
-    $("#test").mouseover(function() {
+    $('#test').mouseover(function () {
         $('h3').addClass('onHoverRed');
     });
-    $("#test").mouseleave(function() {
+    $('#test').mouseleave(function () {
         $('h3').removeClass('onHoverRed');
     });
 

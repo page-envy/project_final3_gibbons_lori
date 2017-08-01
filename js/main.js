fa243feff99eeae2033c449a1e66b8bea9d8b39c
Lori Gibbons <lori.j.gibbons@gmail.com>
2017-07-31 20:14:46 -0400

    Change comments in JQuery

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Custom Javascript Code
 //This should flip between two images upon hover of image in header in contact page
-// var phoneImage = document.getElementById('monsterPhonePic');
-//
+var phoneImage = document.getElementById('monsterPhonePic');
+
 var phoneImageWithFood = document.getElementById('monsterPhonePicWithFood');
 
 phoneImage.addEventListener('mouseover', function () {
@@ -13,7 +13,7 @@ phoneImageWithFood.addEventListener('mouseleave', function () {
     phoneImageWithFood.className = 'hide';
 });
 
-//JQuery Code
+//JQuery Code Start
 $(document).ready(function () {
     //Top to Bottom Plugin, Elevator
     //http://www.jqueryscript.net/animation/jQuery-Plugin-For-Scroll-To-Top-Bottom-Buttons-elevator.html
@@ -43,6 +43,9 @@ $(document).ready(function () {
     //Backstretch Plugin
     //Second of the two required to use
     $('#background-image').backstretch('http://dl.dropbox.com/u/515046/www/garfield-interior.jpg');
+
+
+
     //Custom JQuery
     //This code will make paragraph text in about page red on mouseover
     $('#test').mouseover(function () {
@@ -53,13 +56,16 @@ $(document).ready(function () {
     });
 
 
-    // TESTING
-    //$('p').hover(
-    //     function(
-    //          $( 'h3' ).append( $( "<span> ***</span>" ) );
-    //     ) {},
-    //     function(
-    //          $( 'h3' ).find( "span:last" ).remove();
-    //     ) {}
-    // );
+
+
+    // Custom JQuery
+    // On Menu Page, this should add a span with **** upon hover of the h3 elements on specifically the menu page
+    $('article.h3').hover(
+        function(
+             $( 'h3' ).append( $( "<span> ***</span>" ) );
+        ) {},
+        function(
+             $( 'h3' ).find( "span:last" ).remove();
+        ) {}
+    );
 });
